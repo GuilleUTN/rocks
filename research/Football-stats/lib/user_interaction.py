b5e1957f0b29a2cd3098200a9c502adb9a94f852
JulianGhi <ghiglieri.julian@gmail.com>
2018-05-27 18:39:31 -0300

    add folder with research projects

---
+++ research/Football-stats/lib/user_interaction.py
@@ -0,0 +1,33 @@
+import cv2
+
+def getPerpectiveCoordinates(image, windowName, mouse):
+	"""Get coordinates of 4 points by providing them with mouse clicks.
+	
+	Args:
+		image: Image.
+		windowName (string): Name of the window showing the image.
+		mouse: Mouse object.
+
+	Returns:
+		list: An array of 4 items holding coordinate tuples (x,y).
+
+	"""
+	cv2.imshow(windowName, image)	
+	cv2.setMouseCallback(windowName, mouse.leftClick)
+	
+	i = 0
+	coords = []
+	
+	while i < 4:
+		i += 1
+		mouse.x = 0
+		mouse.y = 0
+		mouse.leftClicked = False
+		
+		while mouse.leftClicked == False:
+			# wait for key press
+			key = cv2.waitKey(1) & 0xFF
+		
+		coords.append((mouse.x, mouse.y))
+
+	return coords
