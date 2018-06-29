fddea9f46e82dba7b6a07cf438fc5b5e23c12064
Guillermo Gaete <guillermogaete49@gmail.com>
2018-06-29 18:17:03 -0300

    Add mobile-app

---
+++ mobile-app/my-awesome-app/store/initialState/main.js
@@ -0,0 +1,14 @@
+const initialState={
+      logging:false,
+      logged:false,
+      response:null,
+      logouting:false,
+      error:{
+        message:null,
+        code:null
+      }
+}
+
+module.exports={
+  initialState
+}
