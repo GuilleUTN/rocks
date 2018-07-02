458ac22f4514ae10904ee393bda7a90e6aef07e9
Isaac Guillermo Gaete <guillermogaete49@gmail.com>
2018-07-01 22:02:38 -0300

    Rename folder

--- mobile-app/app/App.js
+++ mobile-app/app/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Main from './components/main';
-import { Provider } from "react-redux"
-import store from "./store/index"
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Main />
-        </View>
-      </Provider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
