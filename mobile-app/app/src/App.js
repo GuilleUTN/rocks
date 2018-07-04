69ec0440ae054a0f3aef066e2510ecda6f9ad34d
Isaac Guillermo Gaete <guillermogaete49@gmail.com>
2018-07-03 21:46:41 -0300

    Refactor

--- mobile-app/app/src/App.js
+++ mobile-app/app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Main from './components/main.js';
-
 export default class App extends React.Component {
   render() {
     return (
-      <Main></Main>
+      <View style={styles.container}>
+        <Text>Open up src/App.js to start working on your app!</Text>
+      </View>
     );
   }
 }
